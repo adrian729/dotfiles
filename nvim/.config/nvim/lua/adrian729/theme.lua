257aee166df413fbf9c4790a8b5717dd94fc586e
ADRIAN SANCHEZ ALBANELL <adrian.sanchez.albanell@daedalus.de>
2024-11-05 11:14:25 +0100

    small theme changes

--- nvim/.config/nvim/lua/adrian729/theme.lua
+++ nvim/.config/nvim/lua/adrian729/theme.lua
@@ -1,4 +1,15 @@
 -- Color schemes should be loaded after plug#end() (plugins.lua).
+
+-- Overwritte colors here
+require("catppuccin").setup {
+    color_overrides = {
+        all = {
+        },
+        mocha = {
+        }
+    }
+}
+
 -- We prepend it with 'silent!' to ignore errors when it's not yet installed.
 vim.cmd('silent! colorscheme catppuccin-mocha') -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 
