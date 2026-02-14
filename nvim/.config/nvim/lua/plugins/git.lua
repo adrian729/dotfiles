1a6e8e264c936d6e84afe6636a1cf27a67d2bc6d
adrian-daedalus <adrian.sanchez.albanell@daedalus.de>
2026-02-14 17:31:52 +0100

    adding fugitive

---
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "tpope/vim-fugitive",
+        cmd = "Git",
+        keys = {
+            {
+                "<leader>gs",
+                vim.cmd.Git,
+                desc = "Fugitive Git Status",
+            }
+        }
+    },
+}
